8d388f4c2fe112c1921a256257544e53a2865938
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-11-21 18:33:28 +0200

    write component typography and style

---
+++ src/Components/Typography/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-size: 2.5rem;
+  line-height: 1.2;
+  font-weight: 400;
+  letter-spacing: 15px;
+  text-transform: uppercase;
+`;
+export const Head1 = styled.h1`
+  font-size: 3.5rem;
+  line-height: 1.2;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin: 10px 0 20px 0;
+`;
+export const PageHead2 = styled.h1``;
+
+export const H4 = styled.h4``;
+
+export const H6 = styled.h6``;
+
+export const P = styled.p`
+  line-height: 1;
+  font-size: 21px;
+  margin: 20px 0;
+`;
