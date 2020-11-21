a10f56a4448c74abd8754ac85bcddbea07e27eed
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-11-21 18:18:17 +0200

    get icons from fontawesome library

---
+++ src/Components/Icons/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Icon = styled.div`
+  margin: 20px 40px;
+  margin-right: 0.5rem;
+`;
+export const ImgIcon = styled.img`
+  height: 2rem;
+  width: 2rem;
+`;
