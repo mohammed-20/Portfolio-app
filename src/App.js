20fc9dc74017c7312938773103174e7a05dabc87
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-11-24 11:42:09 +0200

    create theme color

--- src/App.js
+++ src/App.js
@@ -1,17 +1,100 @@
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  yellow,
+  blue,
+  red,
+  green,
+  mayablue,
+  roman,
+  springgreen,
+  violent,
+  darkBlue,
+  darkCyan,
+  darkGreen,
+  darkIndig,
+  darkOrang,
+  darkPink,
+  darkPurpal,
+  darkRed,
+} from "./Theme";
+import { ThemeProvider } from "styled-components";
 import Home from "./Pages/Home";
+// import AboutUs from "./Pages/AboutUs";
 import "./App.css";
 function App() {
+  const [theme, setTheme] = useState("");
+
+  const handleClick = (e) => {
+    setTheme(e.target.id);
+  };
+
+  const toggleTheme = () => {
+    //Light Color
+    if (theme === "blue") {
+      return blue;
+    }
+    if (theme === "green") {
+      return green;
+    }
+    if (theme === "mayablue") {
+      return mayablue;
+    }
+    if (theme === "red") {
+      return red;
+    }
+    if (theme === "roman") {
+      return roman;
+    }
+    if (theme === "springgreen") {
+      return springgreen;
+    }
+    if (theme === "violent") {
+      return violent;
+    }
+    if (theme === "yellow") {
+      return yellow;
+    }
+    //Dark Color
+    if (theme === "darkBlue") {
+      return darkBlue;
+    }
+    if (theme === "darkCyan") {
+      return darkCyan;
+    }
+    if (theme === "darkGreen") {
+      return darkGreen;
+    }
+    if (theme === "darkIndig") {
+      return darkIndig;
+    }
+    if (theme === "darkOrang") {
+      return darkOrang;
+    }
+    if (theme === "darkPink") {
+      return darkPink;
+    }
+    if (theme === "darkPurpal") {
+      return darkPurpal;
+    }
+    if (theme === "darkRed") {
+      return darkRed;
+    }
+    return yellow;
+  };
+
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          {/* <Route exact path="/About">
-            About
-          </Route>
+      <ThemeProvider theme={toggleTheme()}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home onClick={handleClick} />
+            </Route>
+            {/* <Route exact path="/About">
+              <AboutUs onClick={handleClick} />
+            </Route> */}
+            {/*
           <Route exact path="/Resume">
             Resume
           </Route>
@@ -27,8 +110,9 @@ function App() {
           <Route exact path="/ContactUs">
             ContactUs
           </Route> */}
-        </Switch>
-      </Router>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
