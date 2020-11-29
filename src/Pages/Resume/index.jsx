e97f5151a6c24ab95d6d1ae90b21ed952d4cb966
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-11-29 11:08:21 +0200

    add resume page with style

---
+++ src/Pages/Resume/index.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+import PageTitle from "../../Components/PageTitle";
+import ColorPicker from "../../Components/ColorPicker";
+import Header from "../../Components/Header";
+import Card from "../../Components/Card";
+import * as T from "../../Components/Typography";
+import * as S from "./style";
+export default function Resume() {
+  const [state, setState] = useState("experience");
+
+  return (
+    <S.Wrapper>
+      <ColorPicker />
+      <S.Container>
+        <S.Row>
+          <PageTitle spanhead="My" spantext="My">
+            Resume
+          </PageTitle>
+          <T.Subtitle>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            tenetur ratione quod.
+          </T.Subtitle>
+        </S.Row>
+        <S.TableTitle>
+          <S.Tablelabel onClick={() => setState("experience")}>
+            <S.TableItem className="active">experience</S.TableItem>
+          </S.Tablelabel>
+          <S.Tablelabel onClick={() => setState("education")}>
+            <S.TableItem>education</S.TableItem>
+          </S.Tablelabel>
+        </S.TableTitle>
+        <S.ExperienceContent displayed={state}>
+          <Card
+            dateparagraph="January 2019 to Present"
+            titleCard="Website Developer"
+            subtitlecard="ABC Company"
+            descriptioncard="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+          <Card
+            dateparagraph="February 2018 to 2019"
+            titleCard="Website Designer"
+            subtitlecard="ABC Company"
+            descriptioncard="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+          <Card
+            dateparagraph="January 2016 to 2018"
+            titleCard="UI Developer"
+            subtitlecard="ABC Company"
+            descriptioncard="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+        </S.ExperienceContent>
+        <S.EducationContent displayed={state}>
+          <Card
+            dateparagraph="January 2014 to 2016"
+            titleCard="M.Sc Computer Science"
+            subtitlecard="XYZ University"
+            descriptioncard="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+          <Card
+            dateparagraph="January 2010 to 2014"
+            titleCard="B.Sc Computer Sciencer"
+            subtitlecard="XYZ University"
+            descriptioncard="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+          <Card
+            dateparagraph="January 2008 to 2010"
+            titleCard="Higher School Graduation"
+            subtitlecard="XYZ College"
+            descriptioncard="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+        </S.EducationContent>
+      </S.Container>
+      <Header />
+    </S.Wrapper>
+  );
+}
---
+++ src/Pages/Resume/style.js
@@ -0,0 +1,122 @@
+import styled, { keyframes } from "styled-components";
+const bounceInUp = keyframes`
+  from,
+  60%,
+  75%,
+  90%,
+  to {
+    -webkit-animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+
+  from {
+    opacity: 0;
+    -webkit-transform: translate3d(0, 3000px, 0);
+    transform: translate3d(0, 3000px, 0);
+  }
+
+  60% {
+    opacity: 1;
+    -webkit-transform: translate3d(0, -20px, 0);
+    transform: translate3d(0, -20px, 0);
+  }
+
+  75% {
+    -webkit-transform: translate3d(0, 10px, 0);
+    transform: translate3d(0, 10px, 0);
+  }
+
+  90% {
+    -webkit-transform: translate3d(0, -5px, 0);
+    transform: translate3d(0, -5px, 0);
+  }
+
+  to {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+export const Wrapper = styled.main`
+  /* width: 100%; */
+  height: 100%;
+  padding-top: 120px;
+  padding-bottom: 120px;
+  display: flex;
+  background-color: ${(props) => props.theme.backgroundPages};
+  align-items: center;
+  flex-direction: column;
+  z-index: -2;
+  transition: 0.3s;
+`;
+export const Container = styled.div`
+  padding-right: 15px;
+  width: 100%;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  z-index: 1;
+  transition: 0.3s;
+  animation-fill-mode: both;
+  animation-duration: 1s;
+  animation-name: ${bounceInUp};
+`;
+export const Row = styled.div`
+  margin-bottom: 50px;
+`;
+
+export const TableTitle = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 0;
+  margin-bottom: 30px;
+  list-style: none;
+  border: 0;
+  width: 85%;
+  margin-left: -85px;
+`;
+export const Tablelabel = styled.li`
+  margin-bottom: -1px;
+  width: 50%;
+  background-color: ${(props) => props.theme.backgroundLabel};
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+  transition: all 0.3s ease-in-out;
+  .active {
+    color: ${(props) => props.theme.primary};
+  }
+  &:hover {
+    transform: scale(0.95);
+  }
+`;
+export const TableItem = styled.a`
+  display: block;
+  text-align: center;
+  border-radius: unset;
+  font-weight: 600;
+  border: unset;
+  text-transform: uppercase;
+  cursor: pointer;
+  padding: 10px 0;
+  text-decoration: none;
+  ${(props) => (props.displayed === "experience" ? ".active" : "")};
+  color: ${(props) => props.theme.fontColor};
+  &:hover {
+    color: ${(props) => props.theme.colorhover};
+  }
+`;
+
+export const ExperienceContent = styled.div`
+  margin: auto;
+
+  ${(props) =>
+    props.displayed === "experience" ? "display:block;" : "display:none;"};
+`;
+export const EducationContent = styled.div`
+  margin: auto;
+
+  ${(props) =>
+    props.displayed === "education" ? "display:block;" : "display:none;"};
+`;
