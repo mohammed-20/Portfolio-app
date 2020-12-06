e08966268e14f2d10192af106912081afd3497e7
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-12-06 11:53:28 +0200

    fix prettir images/index

--- src/images/index.js
+++ src/images/index.js
@@ -4,7 +4,7 @@ export { default as Banner } from "./banner.jpg";
 export { default as BgHome } from "./slider-1.jpg";
 export { default as Testimonialsbg } from "./testimonials-bg.jpg";
 export { default as Gallery2 } from "./gallery2.jpg";
+export { default as Gallery1 } from "./gallery2.jpg";
 export { default as Blog1 } from "./blog1.jpg";
 export { default as Blog2 } from "./blog2.jpg";
 export { default as Blog3 } from "./blog3.jpg";
-
