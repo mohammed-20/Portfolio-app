510839aedac774f964ca2162ef484f22429b1513
mohammed-20 <31156267+mohammed-20@users.noreply.github.com>
2020-12-06 12:10:45 +0200

    Image index (#43)
    * add img in index file to gallery file
    
    change header logo and favicon
    
    * fix prettir images/index

--- src/Pages/AboutUs/InfoSection.jsx
+++ src/Pages/AboutUs/InfoSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import * as T from "../../Components/Typography";
-import Banner from "../../images/banner.jpg";
+import { Banner } from "../../images";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHeart,
