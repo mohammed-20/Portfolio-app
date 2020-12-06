2f9c28b4a7684faa5c3f358e8148cb0be8e5f827
mohammed-20 <31156267+mohammed-20@users.noreply.github.com>
2020-12-06 12:11:03 +0200

    add title of navitem with style (#44)

--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -7,7 +7,6 @@ import {
   faFileAlt,
   faListUl,
   faImage,
-  faDollarSign,
   faQuoteLeft,
   faPhoneAlt,
 } from "@fortawesome/free-solid-svg-icons";
@@ -18,30 +17,37 @@ export default function NavBar() {
     <S.NavBar>
       <S.NavItme exact to="/">
         <FontAwesomeIcon icon={faHome} />
+        <S.Title className="title">Home</S.Title>
       </S.NavItme>
 
       <S.NavItme to="/About">
         <FontAwesomeIcon icon={faInfo} />
+        <S.Title className="title">About</S.Title>
       </S.NavItme>
 
       <S.NavItme to="/Resume">
         <FontAwesomeIcon icon={faFileAlt} />
+        <S.Title className="title">Resume</S.Title>
       </S.NavItme>
 
       <S.NavItme to="/Services">
         <FontAwesomeIcon icon={faListUl} />
+        <S.Title className="title">Services</S.Title>
       </S.NavItme>
 
       <S.NavItme to="/Portfolio">
         <FontAwesomeIcon icon={faImage} />
+        <S.Title className="title">Portfolio</S.Title>
       </S.NavItme>
 
       <S.NavItme to="/Testimoials">
         <FontAwesomeIcon icon={faQuoteLeft} />
+        <S.Title className="title">Testimoials</S.Title>
       </S.NavItme>
 
       <S.NavItme to="/ContactUs">
         <FontAwesomeIcon icon={faPhoneAlt} />
+        <S.Title className="title">Contact</S.Title>
       </S.NavItme>
     </S.NavBar>
   );
