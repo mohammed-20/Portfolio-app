bbb5ecd882844a71e4b4420b97c1e001f39c9145
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-12-13 11:31:31 +0200

    add validation in input of contactus

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import * as S from "./style";
 export default function Input(props) {
-  const { name, value, placeholder, type, id } = props;
+  const { name, value, placeholder, type, id, handleChange, error } = props;
   return (
     <S.Divinput>
-      <label htmlFor={id}></label>
+      <label htmlFor={id} />
       <S.Input
         type={type}
         name={name}
         id={id}
         value={value}
         placeholder={placeholder}
+        onChange={handleChange}
       />
+      {error && <S.Error>{error}</S.Error>}
     </S.Divinput>
   );
 }
--- src/animation/index.js
+++ src/animation/index.js
@@ -1,5 +1,6 @@
 export { default as BounceInUp } from "./BounceInUp";
 export { default as FadeInDownBig } from "./FadeInDownBig";
+export { default as FadeRight } from "./FadeRight";
 export { default as Fade } from "./Fade";
 export { default as ZoomIn } from "./ZoomIn";
 export { default as ZoomOut } from "./ZoomOut";
